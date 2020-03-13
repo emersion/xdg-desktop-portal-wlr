9626411350201dfcf78f313e3d8cb044827b0bd2
Dan Shick <dan.shick@gmail.com>
2020-03-13 18:19:19 -0400

    Add pipewire 0.3 support. Workaround pipewire bugs.

--- include/pipewire_screencast.h
+++ include/pipewire_screencast.h
@@ -2,11 +2,13 @@
 #define PIPEWIRE_SCREENCAST_H
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <pipewire/pipewire.h>
-#include <spa/param/format-utils.h>
+#include <spa/utils/result.h>
 #include <spa/param/props.h>
+#include <spa/param/format-utils.h>
 #include <spa/param/video/format-utils.h>
-#include <spa/support/type-map.h>
+
 #include "wlr_screencast.h"
 #include "screencast_common.h"
 #include "xdpw.h"
--- src/screencast/screencast_common.c
+++ src/screencast/screencast_common.c
@@ -1,36 +1,36 @@
 #include "screencast_common.h"
 
-uint32_t pipewire_from_wl_shm(void *data) {
+enum spa_video_format pipewire_from_wl_shm(void *data) {
 	struct screencast_context *ctx = data;
 
 	if (ctx->forced_pixelformat) {
 		if (strcmp(ctx->forced_pixelformat, "BGRx") == 0) {
-			return ctx->type.video_format.BGRx;
+			return SPA_VIDEO_FORMAT_BGRx;
 		}
 		if (strcmp(ctx->forced_pixelformat, "RGBx") == 0) {
-			return ctx->type.video_format.RGBx;
+			return SPA_VIDEO_FORMAT_RGBx;
 		}
 	}
 
 	switch (ctx->simple_frame.format) {
 	case WL_SHM_FORMAT_ARGB8888:
-		return ctx->type.video_format.BGRA;
+		return SPA_VIDEO_FORMAT_BGRA;
 	case WL_SHM_FORMAT_XRGB8888:
-		return ctx->type.video_format.BGRx;
+		return SPA_VIDEO_FORMAT_BGRx;
 	case WL_SHM_FORMAT_RGBA8888:
-		return ctx->type.video_format.ABGR;
+		return SPA_VIDEO_FORMAT_ABGR;
 	case WL_SHM_FORMAT_RGBX8888:
-		return ctx->type.video_format.xBGR;
+		return SPA_VIDEO_FORMAT_xBGR;
 	case WL_SHM_FORMAT_ABGR8888:
-		return ctx->type.video_format.RGBA;
+		return SPA_VIDEO_FORMAT_RGBA;
 	case WL_SHM_FORMAT_XBGR8888:
-		return ctx->type.video_format.RGBx;
+		return SPA_VIDEO_FORMAT_RGBx;
 	case WL_SHM_FORMAT_BGRA8888:
-		return ctx->type.video_format.ARGB;
+		return SPA_VIDEO_FORMAT_ARGB;
 	case WL_SHM_FORMAT_BGRX8888:
-		return ctx->type.video_format.xRGB;
+		return SPA_VIDEO_FORMAT_xRGB;
 	case WL_SHM_FORMAT_NV12:
-		return ctx->type.video_format.NV12;
+		return SPA_VIDEO_FORMAT_NV12;
 	default:
 		abort();
 	}
