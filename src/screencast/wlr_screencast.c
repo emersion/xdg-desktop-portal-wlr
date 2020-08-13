24a793135cef2a16aef17eea291be25c2c9d90c2
columbarius <co1umbarius@protonmail.com>
2020-08-14 01:47:44 +0200

    Add comments to wlr_frame_buffer_clear in wlr_screencast.c

--- src/screencast/wlr_screencast.c
+++ src/screencast/wlr_screencast.c
@@ -20,10 +20,12 @@
 #include "logger.h"
 
 static void wlr_frame_buffer_clear(struct xdpw_screencast_instance *cast) {
+	// This check isn't needed
 	if (cast->simple_frame.data != NULL) {
 		munmap(cast->simple_frame.data, cast->simple_frame.size);
 		cast->simple_frame.data = NULL;
 	}
+	// This one is
 	if (cast->simple_frame.buffer != NULL) {
 		wl_buffer_destroy(cast->simple_frame.buffer);
 		cast->simple_frame.buffer = NULL;
