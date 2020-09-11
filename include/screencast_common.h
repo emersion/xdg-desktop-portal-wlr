220d245edde063caf8b3746508b8918a3ee9e784
columbarius <co1umbarius@protonmail.com>
2020-09-11 18:07:57 +0200

    Rename xdpw_frame to xdpw_sreencopy_frame in screencast_common.h

--- include/screencast_common.h
+++ include/screencast_common.h
@@ -35,12 +35,14 @@ struct xdpw_frame_damage {
 	uint32_t height;
 };
 
-struct xdpw_frame {
+struct xdpw_screencopy_frame {
+	// xdpw_simple_frame
 	uint32_t width;
 	uint32_t height;
 	uint32_t size;
 	uint32_t stride;
 	bool y_invert;
+
 	uint64_t tv_sec;
 	uint32_t tv_nsec;
 	enum wl_shm_format format;
